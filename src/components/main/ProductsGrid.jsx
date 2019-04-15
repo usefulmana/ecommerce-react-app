127e2612b6d94c8a4ff40629abd24dbb9d127e46
Alex Nguyen <alex.nguyen3141@gmail.com>
2019-04-15 23:46:15 +0700

    Made some progress with the product Details page. need more work

--- src/components/main/ProductsGrid.jsx
+++ src/components/main/ProductsGrid.jsx
@@ -4,25 +4,115 @@ import {ProductConsumer} from './context'
 import Header from '../secondary/Header';
 import NavBar from '../secondary/NavBar';
 import Footer from '../secondary/Footer';
+import { Link } from 'react-router-dom';
+import UpButton from '../secondary/UpButton';
+import Product from '../secondary/Product';
+const url = 'http://rmit.chickenkiller.com:8080/products';
 export default class ProductsGrid extends Component {
+    constructor() {
+        super();
+        this.state = {
+            product: [],
+            id: '',
+            name: '',
+            brand: '',
+            price: '',
+            description:'',
+            pageOfItems: []
+        };
+        this.onChangePage = this.onChangePage.bind(this);
+    }
+    onChangePage(pageOfItems) {
+        // update state with new page of items
+        this.setState({ pageOfItems: pageOfItems });
+    }
+    fetchProduct() {
+        fetch(url)
+            .then(res => res.json())
+            .then(json => this.setState({ product: json }));
+    }
+    componentDidMount() {
+        this.fetchProduct();
+    }
+
   render() {
+      console.log(this.state)
     return (
-    <React.Fragment>
+    <ProductsGridWrapper>
         <Header />
         <NavBar />
-        <div className="py-5">
-            <div className="container">
-                <div className="row">
-                    <ProductConsumer>
-                        {(value)=>{
-                            return <h1>{value}</h1>
-                        }}
-                    </ProductConsumer>
+            <div className="row bg-light">
+                <div className="col-lg-4 text-muted">
+                    <p> ALL PRODUCTS </p>
+                </div>
+                <div className="col-lg-8 text-right">
+                    <Link to="/">
+                        <a className="text-muted">HOME</a>
+                    </Link>
+                    /
+            <Link to="/viewProducts">
+                        <a className="text-muted">ALL PRODUCTS</a>
+                    </Link>
                 </div>
             </div>
-        </div>
+            <div>
+                <hr />
+                <div className="views">
+                    <Link to="/viewProducts">
+                        <button
+                            className="btn text-muted"
+                            type="button"
+                            data-toggle="tooltip"
+                            title="Grid View"
+                        >
+                            <i className="fas fa-th" />
+                        </button>
+                    </Link>
+                    <Link to="/viewProductList">
+                        <button
+                            className="btn text-muted"
+                            type="button"
+                            data-toggle="tooltip"
+                            title="List View"
+                        >
+                            <i className="fas fa-list" />
+                        </button>
+                    </Link>
+                </div>
+                <hr />
+            </div>
+            <div>
+                {this.state.product.map(p => (
+                    <Product
+                        name={p.name}
+                        price={p.price}
+                        brand={p.brand}
+                        imageUrl={p.imageUrl}
+                        description={p.description}
+                    />
+                ))}
+            
+            </div>
+        <UpButton/>
         <Footer/>
-    </React.Fragment>
+    </ProductsGridWrapper>
     )
   }
 }
+
+
+const ProductsGridWrapper = styled.div`
+.col-lg-4 {
+    padding-top:2rem;
+    padding-bottom:1rem;
+    padding-left:4rem;
+}
+.col-lg-8{
+    padding-top:2rem;
+    padding-bottom:1rem;
+    padding-right:4rem;
+}
+.col-lg-8 a{
+    margin-left:0.2rem;
+    margin-right:0.2rem;
+}`
--- src/components/secondary/Product.jsx
+++ src/components/secondary/Product.jsx
@@ -1,11 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Image from 'react-image-resizer';
+import styled from 'styled-components';
+import ProductDetail from '../main/ProductDetail';
 
 export default class Product extends Component {
   render() {
+    console.log(this.props)
     return (
       <div>
-        
+        <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+          <div className="card">
+            <Image
+              src={this.props.imageUrl}
+              height={100}
+              width={80}
+              className="card-img-top img-container"
+            />
+            <div className="card-body">
+              <h5 className="card-title text-primary">{this.props.name}</h5>
+               <button className="btn btn-primary" type="button">
+                  {' '}
+                  Details <i className="fa fas-info-circle" />
+                </button>
+              ${this.props.price}
+            </div>
+          </div>
+        </ProductWrapper>
       </div>
     )
   }
 }
+
+const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+    transition: all 1s linear;
+  }
+  .card-footer {
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;
+  }
+  &:hover {
+    .card {
+      border: 0.04rem solid rgba(0, 0, 0, 0.2);
+      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+    }
+    .card-footer {
+      background: rgba(247, 247, 247);
+    }
+  }
+  .img-container{
+    position: relative;
+    overflow: hidden;
+  }
+  .img-container:hover .card-img-top{
+    transform: scale(1.2)
+  }
+`;
